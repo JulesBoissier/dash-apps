3267d5372e5d63d7ba14ea6713fa66af7ba4aa8f
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2024-12-13 16:57:26 +0000

    Updated app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import dash_design_kit as ddk
-from dash import Dash
+from dash import Dash, html
 
 app = Dash(__name__)
 server = app.server  # expose server variable for Procfile
 
-app.layout = ddk.App()
+app.layout = html.Div(
+    
+    children=["An update"]
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
